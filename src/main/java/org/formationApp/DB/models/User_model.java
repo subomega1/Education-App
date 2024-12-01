6706f51fdb882430c9954927e22c377b97994531
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2024-12-01 15:14:02 +0100

    little backend

---
+++ src/main/java/org/formationApp/DB/models/User_model.java
@@ -0,0 +1,24 @@
+package org.formationApp.DB.models;
+
+public abstract class User_model {
+    private String username,email , password ;
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setEmail(String email){
+        this.email= email;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/org/formationApp/DB/models/student.java
@@ -0,0 +1,4 @@
+package org.formationApp.DB.models;
+
+public class student  extends User_model{
+}
