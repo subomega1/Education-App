b6b7f06a852e5f5230f46f6a5cdf4cc222b6b2a7
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2024-12-10 10:19:36 +0100

    still need to implement  the add course  and backend

--- src/main/java/org/formationApp/UI/components/AddCourse.java
+++ src/main/java/org/formationApp/UI/components/AddCourse.java
@@ -1,23 +1,27 @@
 package org.formationApp.UI.components;
 
+import org.formationApp.UI.Theme_Resources.Design_Assets;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AddCourse {
     public AddCourse(){
         JFrame addCourseFrame = new JFrame("Add Course");
         addCourseFrame.setSize(500,500);
         addCourseFrame.setLocationRelativeTo(null);
-        addCourseFrame.getContentPane().setBackground(Color.CYAN);
+        addCourseFrame.getContentPane().setBackground(new Color(Design_Assets.BlackColor.r,Design_Assets.BlackColor.g,Design_Assets.BlackColor.b));
         addCourseFrame.setResizable(false);
-        addCourseFrame.setLayout(new FlowLayout(FlowLayout.CENTER,0,25));
+        addCourseFrame.setLayout(new FlowLayout(FlowLayout.CENTER,0,18));
 
 
         JPanel courseTitleContainer = new JPanel();
         courseTitleContainer.setPreferredSize(new Dimension(480,100));
-        courseTitleContainer.setBackground(Color.magenta);
+        courseTitleContainer.setBackground(new Color(Design_Assets.BlackColor.r,Design_Assets.BlackColor.g,Design_Assets.BlackColor.b));
         courseTitleContainer.setLayout(new FlowLayout(FlowLayout.LEFT ,15,15));
         addCourseFrame.add(courseTitleContainer);
 
@@ -25,60 +29,44 @@ public class AddCourse {
 
         JLabel courseTitleLabel = new JLabel("Course Title");
         courseTitleLabel.setFont( new Font("Monospaced", Font.BOLD, 22));
+        courseTitleLabel.setForeground(new Color(Design_Assets.IndigoColor.r,Design_Assets.IndigoColor.g,Design_Assets.IndigoColor.b));
         courseTitleLabel.setPreferredSize(new Dimension(430,20));
         courseTitleContainer.add(courseTitleLabel);
 
-        JTextArea courseTitle = new JTextArea();
-        courseTitle.setPreferredSize(new Dimension(440,30));
-        int maxTitleLine = 1;
+        JTextField courseTitle = new JTextField();
+        courseTitle.setPreferredSize(new Dimension(440,50));
+        courseTitle.setBackground(new Color(Design_Assets.BlackColor.r,Design_Assets.BlackColor.g,Design_Assets.BlackColor.b));
+        courseTitle.setForeground(Color.white);
+        courseTitle.setCaretColor(Color.white);
+        courseTitle.setBorder(Design_Assets.IndigoBorder.border);
         courseTitle.setFont(new Font("Monospaced", Font.BOLD, 22));
-        courseTitle.setLineWrap(true); // enable line wrapping
-        courseTitle.setWrapStyleWord(true);
+
         courseTitleContainer.add(courseTitle);
-        courseTitle.getDocument().addDocumentListener(new DocumentListener() {
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                checkLines();
-            }
 
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                checkLines();
-            }
-
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                checkLines();
-
-            }
-            private void checkLines(){
-                int lines = courseTitle.getLineCount();
-                if (lines>maxTitleLine){
-                    courseTitle.setCaretPosition(courseTitle.getDocument().getLength());
-                    Toolkit.getDefaultToolkit().beep();
-                }
-            }
-        });
 
 
         JPanel courseDescriptionContainer = new JPanel();
         courseDescriptionContainer.setPreferredSize(new Dimension(480,250));
-        courseDescriptionContainer.setBackground(Color.magenta);
+        courseDescriptionContainer.setBackground(new Color(Design_Assets.BlackColor.r,Design_Assets.BlackColor.g,Design_Assets.BlackColor.b));
         courseDescriptionContainer.setLayout(new FlowLayout(FlowLayout.LEFT ,15,15));
         addCourseFrame.add(courseDescriptionContainer);
 
         JLabel courseDescriptionLabel = new JLabel("Course Description");
         courseDescriptionLabel.setFont( new Font("Monospaced", Font.BOLD, 22));
-        courseDescriptionLabel.setPreferredSize(new Dimension(430,20));
+        courseDescriptionLabel.setPreferredSize(new Dimension(430,40));
         courseDescriptionContainer.add(courseDescriptionLabel);
+        courseDescriptionLabel.setForeground(new Color(Design_Assets.IndigoColor.r,Design_Assets.IndigoColor.g,Design_Assets.IndigoColor.b));
 
 
         JTextArea courseDescription = new JTextArea();
-        courseDescription.setCaretColor(Color.black);
+        courseDescription.setCaretColor(Color.white);
+        courseDescription.setBorder(Design_Assets.IndigoBorder.border);
+        courseDescription.setBackground(new Color(Design_Assets.BlackColor.r,Design_Assets.BlackColor.g,Design_Assets.BlackColor.b));
         courseDescription.setPreferredSize(new Dimension(430,180));
         courseDescription.setFont( new Font("Monospaced", Font.BOLD, 22));
         courseDescription.setLineWrap(true);
         courseDescription.setWrapStyleWord(true);
+        courseDescription.setForeground(Color.white);
         courseDescriptionContainer.add(courseDescription);
         int maxDescriptionLines = 6;
         courseDescription.getDocument().addDocumentListener(new DocumentListener() {
@@ -104,6 +92,24 @@ public class AddCourse {
                 }
             }
         });
+
+        JButton courseAddButton = new JButton();
+        courseAddButton.setPreferredSize(new Dimension(450 ,40));
+        courseAddButton.setFont( new Font("Monospaced", Font.BOLD, 22));
+        courseAddButton.setText("Add course");
+        courseAddButton.setBackground(new Color(Design_Assets.BlackColor.r,Design_Assets.BlackColor.g,Design_Assets.BlackColor.b));
+        courseAddButton.setForeground(new Color(Design_Assets.IndigoColor.r,Design_Assets.IndigoColor.g,Design_Assets.IndigoColor.b));
+        courseAddButton.setBorder(Design_Assets.IndigoBorder.border);
+
+
+        addCourseFrame.add(courseAddButton);
+        courseAddButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              addCourseFrame.setVisible(false);
+            }
+        });
+
 
 
 
